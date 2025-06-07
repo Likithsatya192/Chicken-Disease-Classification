536f2640a55e5054b4141e7c8d93c730a2e742f7
Likith <likithsatya192@gmail.com>
2025-06-07 20:39:04 +0530

    DataIngestion Completed

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from CNNclassifier import logger
+from CNNclassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 
-logger.info("Welcome to my custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
